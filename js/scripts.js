7d2e7841b7b114d2042a4f382fcc82a1a32f8d55
Kayden Smith <kaydensmith@kaydens-air.wifi.uleth.ca>
2023-10-10 13:39:33 -0600

    changed

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,37 @@ document.getElementById("prev").style.background= "4a4a4a"
 
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
-const APODs = document.getElementsByClassName("APOD");
-console.log(APODs);
+//const APODs = document.getElementsByClassName("APOD");
+//console.log(APODs);
 let index = 0;
+
+function APOD_obj(source, date, image, title) {
+    this.source = source,
+    this.date = date,
+    this.image = image,
+    this.title = title
+}
+const APOD_oct3 = new APOD_obj("Image Credit & Copyright: NASA, ESA, Hubble, HLA; Processing & Copyright: Harshwardhan Pathak", "03-10-2023", "images/Hourglass_HubblePathak_1080.jpg", "MyCn 18: The Engraved Hourglass Planetary Nebula")
+const APOD_oct4 = new APOD_obj("Image Credit & Copyright: Abdullah Alharbi", "04-10-2023", "images/WitchHead_Alharbi_1080.jpg", "The Witch Head Nebula")
+const APOD_oct5 = new APOD_obj("Image Credit & Copyright: Tunc Tezel (TWAN)", "05-10-2023", "images/MoValleyEclipse1024.jpg", "Ring of Fire over Monument Valley")
+
+const APOD_array = [APOD_oct3, APOD_oct4, APOD_oct5];
+console.log(APOD_array);
+
+function createAPOD() {
+    const APOD = document.createElement("div");
+    APOD.className = "APOD";
+    APOD.innerHTML = "<figure>"
+    + "<img src='" + APOD_array[index].image + "'>"
+    + "<p>" + APOD_obj.source + "</p>"
+    + "</figure>"
+    + "<div><p>" + APOD_array[index].date + "</p>"
+    + "<h2>" + APOD_array[index].title + "</h2>";
+    document.getElementById("APOD__group").innerHTML = "";
+    document.getElementById("APOD__group").appendChild(APOD);
+}
+
+createAPOD();
 
 prev.innerHTML = "&larr; go back";
 
@@ -15,17 +43,19 @@ next.addEventListener("click", goforward);
 
 function goback() {
     console.log("go back");
-    APODs[index].style.display = "none";
+    //APODs[index].style.display = "none";
     index++;
-    APODs[index].style.display = "grid";
+    //APODs[index].style.display = "grid";
     updateControls();
+    createAPOD();
 }
 function goforward() {
     console.log("go forward");
-    APODs[index].style.display = "none";
+    //APODs[index].style.display = "none";
     index--;
-    APODs[index].style.display = "grid";
+    //APODs[index].style.display = "grid";
     updateControls();
+    createAPOD();
 }
 
 function updateControls() {
@@ -34,7 +64,7 @@ function updateControls() {
     } else {
         next.style.visibility = "visible";
     }
-    if (index == APODs.length - 1) {
+    if (index == APOD_array.length - 1) {
         prev.style.visibility = "hidden";
     } else {
         prev.style.visibility = "visible";
